d4abfa4c22da36fe19b7e5fe320f0e4b25664829
Xander <alexandermoreno.dev@gmail.com>
2023-04-09 14:45:56 -0500

    prueba de levantar docker

--- init.sh
+++ init.sh
@@ -3,12 +3,24 @@
 # Copia el archivo .env.template y lo renombra como .env
 cp .env.template .env
 
+# Limpia los valores en las variables del archivo .env.template
+sed -i 's/=.*$/=/' .env.template
+
 # Elimina la carpeta .git
 rm -rf .git
 
 # Inicializa un nuevo repositorio Git
 git init
 
+#ejecuta docker
+docker-compose up -d
+
+# Elimina el archivo
+rm .gitattributes
+
 # Realiza un commit inicial
 git add .
-git commit -m "Commit inicial"
+git commit -m "First comit"
+
+# Elimina el archivo init.sh
+rm -- "$0"
